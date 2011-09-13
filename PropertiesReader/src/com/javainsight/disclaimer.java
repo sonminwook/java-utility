3b25e133f6fae99024988195e7654fd8f4849054
xesunny <xesunny@gmail.com>
2011-09-13 05:50:59 +0000

    changed the license agreement..

--- PropertiesReader/src/com/javainsight/disclaimer.java
+++ PropertiesReader/src/com/javainsight/disclaimer.java
@@ -8,7 +8,7 @@ public class disclaimer {
 	private static Logger logger = Logger.getLogger(disclaimer.class);
 	public static void print(){
 		String disclaimer = "~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*\n" +
-				"This SOFTWARE PRODUCT is provided by Java~Insight (http://jovialjava.blogspot.com) " +
+				"This SOFTWARE PRODUCT is provided by Java~Insight (http://javainsight.wordpress.com) " +
 				"\"as is\" and \"with all faults.\" \n THE PROVIDER makes no representations" +
 				" or warranties of any kind concerning the safety, suitability, lack of viruses, " +
 				"inaccuracies, typographical errors, or other harmful components of this SOFTWARE PRODUCT.\n " +
@@ -17,8 +17,8 @@ public class disclaimer {
 				"other software installed on your equipment.\n You are also solely responsible for the protection " +
 				"of your equipment and backup of your data, and THE PROVIDER will not be liable for any damages you" +
 				" may suffer in connection with using, modifying, or distributing this SOFTWARE PRODUCT.\n"+
-				"This SOFTWARE PRODUCT is FREE to use for single user, For distribution purpose and before any modification" +
-				"PLEASE TAKE PERMISSION " + "javainsights@gmail.com"+
+				"This SOFTWARE PRODUCT is FREE to use/distribute without any modification for Global Blue organization for its product Pay@Table.\n" +
+				"For any modification please contact us at [" + "javainsights@gmail.com]"+
 				"~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*\n";
 		logger.fatal(disclaimer);
 	}
