4b12117a44abd827dde58e9283d49cd57250ea0a
xesunny@gmail.com <xesunny@gmail.com@0256394b-a956-e3e0-2bb8-f2f0d239d379>
2012-03-04 08:30:14 +0000

    cloud stuff added

---
+++ trunk/PropertiesReader/src/com/javainsight/cloud/Constants.java
@@ -0,0 +1,15 @@
+package com.javainsight.cloud;
+
+public class Constants {
+
+	public static String GOOGLE_DOC_URL = "https://docs.google.com/feeds/default/private/full/";
+	public static String EXCEL 	  = "-/spreadsheet";
+	public static String DOC 		  = "-/document";
+	public static String PPT 		  = "-/presentation/mine";
+	public static String FOLDER 	  = "-/folder";
+	
+	public static String AUTH_TOKEN = "DQAAAKAAAABsRsSvYr5DC_a0K9mfde2R0bMe2BoKWB--sQ3yJxAjzp6IL" +
+								"4NxUkQoN58cSfCVVnCWm7QH-KNs7ElXLqgAaCwu8mLyBs3yXOGOO7vArQw5Xu3N1X5WycB5" +
+								"qwx1eeobZkQRntqNGiFOhTNq0amrqbYJBaSpKugPCC0nq4g55UgCBf75m0uMlpPES68-r4hsDAq" +
+								"ZBJjvc-ilxWmNMwVhtYWf";
+}
